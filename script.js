2bb5cc186af9edcf79294fe93b32bed6d70c0f6f
isa marchiori <126196072+arthyxn@users.noreply.github.com>
2023-11-17 08:08:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -47,6 +47,42 @@ function calcula(){
     executa = "";
   }
 }
+let a = 0;
+let b = 0;
+let c = 0;
+function calcula_eq(){
+     if(a != 0){
+       let delta = (b*b) - (4*a*c); 
+       document.getElementById("delta").innerHTML = delta;
+     }
+}
+function muda_sinal_a(){
+    a = (-1)*a;
+    calcula_eq();
+}
+function muda_sinal_b(){
+    b = (-1)*b;
+    calcula_eq();
+}
+function muda_sinal_c(){
+    c = (-1)*c;
+    calcula_eq();
+}
+function set_valor_a(){
+    a = document.getElementById("valor_a").value;
+    calcula_eq();
+} 
+function set_valor_b(){
+  b = document.getElementById("valor_b").value;
+  calcula_eq();
+}
+function set_valor_c(){
+  c = document.getElementById("valor_c").value;
+  calcula_eq();
+}
+
+
+
 
 mensagem(soma(15,soma(11,soma(4,7))));
 mensagem(sub(18,7));
